b68677e357dc2039785fc6be9d1caadef0709392
chinmaykrishna <seekay3@gmail.com>
2019-06-07 21:45:42 +0530

    moved behance from top to bottom

---
+++ public/js/recorder.js
@@ -0,0 +1,20 @@
+
+/*let events = [];
+
+rrweb.record({
+  emit(event) {
+    // push event into the events array
+    events.push(event);
+  },
+});
+
+// this function will send events to the backend and reset the events array
+function save() {
+  const body = JSON.stringify({ events });
+  console.log(body);
+  events = [];
+
+}
+
+// save events every 10 seconds
+setInterval(save, 10 * 1000);*/
